613f7428c99f9b3a5b2c4854f5b2a097b9bcdcf6
Scpine8 <Scpine8@gmail.com>
2020-11-19 02:06:31 -0800

    feat: erased src, added home.js

---
+++ my-app/src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+}
+
+export default Home;
