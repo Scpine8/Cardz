782250b2b7d194c9e8ac1084c4bb6b9a4df89f47
Sean Pine <scpine8@gmail.com>
2020-11-19 23:02:40 -0800

    fix: resolved merged conflict

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,7 +11,6 @@ class Home extends React.Component {
             thing2: 1
         }
     }
-<<<<<<< Updated upstream
     render() {
         return (
             <div class="accounts-list">
@@ -27,9 +26,6 @@ class Home extends React.Component {
         )
         
     }
-=======
-    
->>>>>>> Stashed changes
 }
 
 ReactDOM.render(
