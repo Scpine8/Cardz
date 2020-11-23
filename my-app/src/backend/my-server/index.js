7a153dafe0c8fb0a2335be07d4f86832b42905c7
Scpine8 <Scpine8@gmail.com>
2020-11-22 22:57:52 -0800

    feat: in my_server, both get and post return list of previously posted accounts

--- my-app/src/backend/my-server/index.js
+++ my-app/src/backend/my-server/index.js
@@ -7,16 +7,18 @@ const port = 8001
 app.use(cors())
 app.use(bodyParser())
 
+let accounts = [];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(accounts.length > 0 ? accounts : "Hello World!")
 })
 
 app.post('/', (req, res) => {
   console.log(req.body)
-  res.send("Data Recieved!")
-  res.sendStatus(200)
+  accounts.push(req.body.account)
+  res.send(accounts.length > 0 ? accounts : "Hello World!")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
 })
