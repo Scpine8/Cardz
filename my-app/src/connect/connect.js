c4b32690c26cc3ed905f6f8c31ebfb0dff42d18c
Scpine8 <Scpine8@gmail.com>
2020-11-22 22:59:12 -0800

    fix: changed the content type of the post to JSON

--- my-app/src/connect/connect.js
+++ my-app/src/connect/connect.js
@@ -16,7 +16,7 @@ class Connect {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             headers: {
-                'Content-Type': 'text'
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(data) // body data type must match "Content-Type" header
         });
