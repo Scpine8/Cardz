e44fc0d6011e28da2780f315c15819ab0be8a5c1
Scpine8 <Scpine8@gmail.com>
2020-12-09 17:46:13 -0800

    test: added structure for Home testing

---
+++ my-app/__tests__/src/index.test.js
@@ -0,0 +1,21 @@
+import Home from '../../src/index';
+
+// Home Component Tests
+
+beforeEach(() => {
+    // mount Home
+});
+afterEach(() => {
+    // unmount Home
+})
+
+describe('handleAccountSelect tests', () => {
+})
+describe('handleGetData tests', () => {
+})
+describe('handleClearData tests', () => {
+})
+describe('handleAccountRemove tests', () => {
+})
+describe('handleAccountSelect test', () => {
+})
